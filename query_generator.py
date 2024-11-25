65894cc3b3e38586a0d2524a57d9cefe5480566e
GK <weeeeek2@naver.com>
2024-11-25 11:47:21 +0900

    fix: Update query_generator

--- query_generator.py
+++ query_generator.py
@@ -6,43 +6,45 @@ start_date = datetime(2025, 1, 1)
 end_date = datetime(2025, 1, 31)
 
 # 항공사 목록
+countries = [['인천', '뉴욕', 13, 3, 700, 2000], ['뉴욕','인천', 14, 3, 700, 2000], ['서울', '부산', 2, 0, 30, 80]]
 airlines = ['대한항공', '아시아나항공', '티웨이항공', '제주항공', '진에어항공']
 
 # INSERT 쿼리 시작
 query = "INSERT INTO Flight (departure_location, arrival_location, departure_date, arrival_date, airline, price) VALUES\n"
 
-rows = []
-current_date = start_date
+for country in countries:
+    departureLocation = country[0]
+    arrivalLocation = country[1]
+    defaultHour = country[2]
+    errorHour = country[3]
+    minPrice = country[4]
+    maxPrice = country[5]
+    rows = []
+    current_date = start_date
 
-while current_date <= end_date:
-    # 랜덤으로 4개의 항공사를 선택
-    daily_airlines = random.sample(airlines, 4)
-    
-    for airline in daily_airlines:
-        # 출발 및 도착 시간 생성
-        departure_to_ny = current_date.strftime("%Y-%m-%d %H:%M")
-        arrival_to_ny = (current_date + timedelta(hours=random.randint(10, 15))).strftime("%Y-%m-%d %H:%M")
-        return_from_ny = (current_date + timedelta(days=1, hours=random.randint(18, 22))).strftime("%Y-%m-%d %H:%M")
-        arrival_from_ny = (current_date + timedelta(days=2, hours=random.randint(5, 8))).strftime("%Y-%m-%d %H:%M")
+    while current_date <= end_date:
+        # 랜덤으로 4개의 항공사를 선택
+        daily_airlines = random.sample(airlines, 4)
         
-        # 가격을 랜덤으로 설정 (1,000,000 ~ 2,000,000, 1만 원 단위)
-        price_to_ny = random.randint(100, 200) * 10000
-        price_from_ny = random.randint(100, 200) * 10000
+        for airline in daily_airlines:
+            # 출발 및 도착 시간 생성
+            randHour = random.randint(0, 23)
+            departureDate = (current_date + timedelta(hours=randHour, minutes=random.randint(0, 11)*5)).strftime("%Y-%m-%d %H:%M")
+            arrivalDate = (current_date + timedelta(hours=randHour+random.randint(defaultHour, defaultHour+errorHour), minutes=random.randint(0, 11)*5)).strftime("%Y-%m-%d %H:%M")
+            price = random.randint(minPrice, maxPrice) * 1000
+            
+            # 데이터 추가
+            rows.append(
+                f"('{departureLocation}', '{arrivalLocation}', '{departureDate}', '{arrivalDate}', '{airline}', {price})"
+            )
+            
         
-        # 데이터 추가
-        rows.append(
-            f"('인천', '뉴욕', '{departure_to_ny}', '{arrival_to_ny}', '{airline}', {price_to_ny})"
-        )
-        rows.append(
-            f"('뉴욕', '인천', '{return_from_ny}', '{arrival_from_ny}', '{airline}', {price_from_ny})"
-        )
-    
-    # 하루 증가
-    current_date += timedelta(days=1)
+        # 하루 증가
+        current_date += timedelta(days=1)
 
-# 쿼리 조합
-query += ",\n".join(rows) + ";"
+    # 쿼리 조합
+    query += ",\n".join(rows) + ";"
 
-# 출력
-print(query)
+    # 출력
+    print(query)
 
